c81c383aa7ada7b52c67560d7d27639a0bbb8fb7
XXORI <ccori.arias@pucp.edu.pe>
2026-07-10 21:31:25 -0500

    Update código.py

--- código.py
+++ código.py
@@ -625,7 +625,7 @@ elif selected == "🧪 Hawkins Lab":
                 # Mostramos la portada guardada en el Excel.
                 st.image(
                     cancion["Portada (imagen)"],
-                    width=120
+                    width=180
                 )
 
             with columna_texto:
